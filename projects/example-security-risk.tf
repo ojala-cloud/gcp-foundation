d909705a0f04b037cb04faedaed1de3e06b0b824
petri-ojala <petri.ojala+github@gmail.com>
2021-11-25 16:20:24 +0200

    example bad code

---
+++ projects/example-security-risk.tf
@@ -0,0 +1,18 @@
+#
+# Example code for terrascan and others
+#
+
+resource "google_storage_bucket" "user_error" {
+  name                        = "bad-gcs-bucket"
+  location                    = "EU"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_binding" "user_error" {
+  bucket = google_storage_bucket.user_error.name
+  role   = "roles/storage.viewer"
+  members = [
+    "allUsers",
+  ]
+}
