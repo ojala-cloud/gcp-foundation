96461941ba25b63e913010ecad8cced75c9d34b3
petri-ojala <petri.ojala+github@gmail.com>
2021-11-25 16:30:34 +0200

    added missing project

--- projects/example-security-risk.tf
+++ projects/example-security-risk.tf
@@ -4,6 +4,7 @@
 
 resource "google_storage_bucket" "user_error" {
   name                        = "bad-gcs-bucket"
+  project                     = "home-infrastructure-254608"
   location                    = "EU"
   force_destroy               = true
   uniform_bucket_level_access = true
